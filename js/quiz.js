e512d954134752fe0406235a68aa12b7926a2c04
Josh Cunningham <joshcunningsworth@gmail.com>
2018-09-17 20:57:03 -0400

    fixed click event bug

--- js/quiz.js
+++ js/quiz.js
@@ -18,7 +18,7 @@
 			var questionState = $scope.myQuestions[qIndex].questionState;
 
 			if(questionState != 'answered'){
-				$scope.myQuestions[qIndex].selectAnswer = aIndex;
+				$scope.myQuestions[qIndex].selectedAnswer = aIndex;
 				var correctAnswer = $scope.myQuestions[qIndex].correct;
 				$scope.myQuestions[qIndex].correctAnswer = correctAnswer;
 
